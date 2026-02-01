5b4cba133ce924bee5210795cf2ee455d9a9982f
Jeyaraman S <jeyaramanapr22@gmail.com>
2026-02-01 09:36:44 +0530

    changed the header style

--- src/page/AccountPage.jsx
+++ src/page/AccountPage.jsx
@@ -99,7 +99,7 @@ const AccountPage = () => {
 
   return(
     <div className="min-h-screen bg-gray-50">
-      <header className="bg-white shadow-sm">
+      <header className="mt-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex items-center justify-between">
             <div>
--- src/page/TransactionPage.jsx
+++ src/page/TransactionPage.jsx
@@ -124,12 +124,12 @@ const TransactionPage = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <header className="bg-white shadow-sm">
+      <header className=" mt-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex items-center justify-between">
             <div>
               <h1 className="text-3xl font-bold text-gray-900">
-                💰 Money Manager
+                💵 Transaction
               </h1>
               <p className="text-sm text-gray-600 mt-1">
                 Manage your finances with ease
